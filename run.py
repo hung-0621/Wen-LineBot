f5a8df3524ab03380b2a0c1a4b5d671eac428c94
powen <powen0966091716@gmail.com>
2024-03-05 00:29:06 +0800

    Update run.py

--- run.py
+++ run.py
@@ -46,14 +46,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
+    if event.message.text == "嗨張子儀" and event.source.type == "group":
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="嗨張子儀")]
+                )
             )
-        )
 
 if __name__ == "__main__":
     app.run()
