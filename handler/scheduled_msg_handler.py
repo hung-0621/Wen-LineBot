aacc27a981979b351b360592a8110d19c56c35e1
powen <powen0966091716@gmail.com>
2024-03-05 11:40:45 +0800

    test3 !!!

--- handler/scheduled_msg_handler.py
+++ handler/scheduled_msg_handler.py
@@ -1,6 +1,54 @@
-class SCHEDULED_HANDLER:
-    def __init__(self):
-        pass
+from apscheduler.schedulers.background import BackgroundScheduler
+from pytz import timezone
+from datetime import datetime
 
-    def main(self):
-        pass
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    PushMessageRequest
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+class SCHEDULED_HANDLER:
+
+    HOUR_MSG_DICT = {
+        3: "三點了 ！！ 起來重睡 ！！",
+        8: "八點了 ！！ 起來早八 ！！",
+        12: "十二點了 ！！ 起來進食 ！！",
+        15: "下午三點了 ！！ 起來喝下午”茶“ ！！",
+    }
+
+    event: MessageEvent
+    line_bot_api: MessagingApi
+    configuration: Configuration
+
+    def __init__(self,event,line_bot_api,configuration):
+        self.event = event
+        self.line_bot_api = line_bot_api
+        self.configuration = configuration
+
+
+    def send_hourly_message(self,group_id, name):
+        hour = datetime.now(timezone('Asia/Taipei')).hour
+        message_text = self.HOUR_MSG_DICT.get(hour,"測試排程訊息")
+        message = TextMessage(text=f"{name} {message_text}")
+        push_message_request = PushMessageRequest(to=group_id, messages=[message])
+        self.line_bot_api.push_message(push_message_request)
+
+    def handle(self):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(self.send_hourly_message, 'cron', minute=0, second=0, timezone=timezone('Asia/Taipei'), args=["Ca910ecfb8c7289e2c5fc51d58189d01c", "張子儀"])
+        scheduler.add_job(self.send_hourly_message, 'cron', second=0, timezone=timezone('Asia/Taipei'), args=["C7f44352f6748f82224d1c211175ae839", "張子儀"])
+        scheduler.start()
--- run.py
+++ run.py
@@ -25,6 +25,7 @@ from linebot.v3.webhooks import (
 
 from handler.msg_handler import MSG_HANDLER
 from handler.cmd_handler import CMD_HANDLER
+from handler.scheduled_msg_handler import SCHEDULED_HANDLER
 
 app = Flask(__name__)
 
@@ -33,26 +34,26 @@ configuration = Configuration(
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET', None))
 
 
-messages = {
-    3: "三點了 ！！ 起來重睡 ！！",
-    8: "八點了 ！！ 起來早八 ！！",
-    12: "十二點了 ！！ 起來進食 ！！",
-    15: "下午三點了 ！！ 起來喝下午”茶“ ！！",
-}
+# messages = {
+#     3: "三點了 ！！ 起來重睡 ！！",
+#     8: "八點了 ！！ 起來早八 ！！",
+#     12: "十二點了 ！！ 起來進食 ！！",
+#     15: "下午三點了 ！！ 起來喝下午”茶“ ！！",
+# }
 
-def send_hourly_message(group_id, name):
-    hour = datetime.now(timezone('Asia/Taipei')).hour
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        message_text = messages.get(hour,"測試排程訊息")
-        message = TextMessage(text=f"{name} {message_text}")
-        push_message_request = PushMessageRequest(to=group_id, messages=[message])
-        line_bot_api.push_message(push_message_request)
+# def send_hourly_message(group_id, name):
+#     hour = datetime.now(timezone('Asia/Taipei')).hour
+#     with ApiClient(configuration) as api_client:
+#         line_bot_api = MessagingApi(api_client)
+#         message_text = messages.get(hour,"測試排程訊息")
+#         message = TextMessage(text=f"{name} {message_text}")
+#         push_message_request = PushMessageRequest(to=group_id, messages=[message])
+#         line_bot_api.push_message(push_message_request)
 
-scheduler = BackgroundScheduler()
-scheduler.add_job(send_hourly_message, 'cron', minute=0, second=0, timezone=timezone('Asia/Taipei'), args=["Ca910ecfb8c7289e2c5fc51d58189d01c", "張子儀"])
-# scheduler.add_job(send_hourly_message, 'cron', second=0, timezone=timezone('Asia/Taipei'), args=["C7f44352f6748f82224d1c211175ae839", "張子儀"])
-scheduler.start()
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(send_hourly_message, 'cron', minute=0, second=0, timezone=timezone('Asia/Taipei'), args=["Ca910ecfb8c7289e2c5fc51d58189d01c", "張子儀"])
+# # scheduler.add_job(send_hourly_message, 'cron', second=0, timezone=timezone('Asia/Taipei'), args=["C7f44352f6748f82224d1c211175ae839", "張子儀"])
+# scheduler.start()
 
 
 
@@ -81,13 +82,17 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
+    scheduled_msg_handler = SCHEDULED_HANDLER()
     cmd_handler = CMD_HANDLER()
     msg_handler = MSG_HANDLER(event=event,configuration=configuration,line_bot_api=line_bot_api,cmd_handler=cmd_handler)
     msg_handler.handle()
+    scheduled_msg_handler.handle()
+
 
 
 if __name__ == "__main__":
     try:
         app.run()
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+    except (KeyboardInterrupt, SystemExit) as e:
+        # scheduler.shutdown()
+        print(e)
