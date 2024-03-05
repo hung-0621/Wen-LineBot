2e4a684848fa64bf0ee8b7b8aa0eafb8b2a3e3c2
powen <powen0966091716@gmail.com>
2024-03-05 23:21:04 +0800

    fuck the code

--- handler/cmd_handler.py
+++ handler/cmd_handler.py
@@ -40,7 +40,7 @@ class CMD_HANDLER:
             original_content_url=url,
             preview_image_url=url
         )
-        self.line_bot_api.reply_message(self.event.reply_token, image_message)
+        self.line_bot_api.reply_message(self.event.reply_token, [image_message])
 
 
 
