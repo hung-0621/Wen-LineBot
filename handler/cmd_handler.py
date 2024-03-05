e316271e960357305ce359136002d0caf9b63e0f
powen <powen0966091716@gmail.com>
2024-03-05 15:53:21 +0800

    Update cmd_handler.py

--- handler/cmd_handler.py
+++ handler/cmd_handler.py
@@ -36,11 +36,11 @@ class CMD_HANDLER:
 
     def send_image(self, url):
         image_message = ImageMessage(
-        original_content_url=url,
-        preview_image_url=url
-    )
-        self.line_bot_api.reply_message(self.event.reply_token, image_message)
-
+            original_content_url=url,
+            preview_image_url=url
+        )
+        self.line_bot_api.reply_message(
+            self.event.reply_token, [image_message])
 
     def send_message(self, msg):
         self.line_bot_api.reply_message_with_http_info(
