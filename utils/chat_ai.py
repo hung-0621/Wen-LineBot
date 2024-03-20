f04f8d1a91497d09ae71c23a65be21db1095347d
powen <powen0966091716@gmail.com>
2024-03-20 23:23:37 +0800

    Update chat_ai.py

--- utils/chat_ai.py
+++ utils/chat_ai.py
@@ -3,11 +3,9 @@ import json
 
 import google.generativeai as genai
 
-GOOGLE_API_KEY = os.getenv('GIMINI_API_KEY', None)
-
 def get_ai_response(message: str) -> str:
     model = genai.GenerativeModel('gemini-pro')
-    genai.configure(api_key=GOOGLE_API_KEY)
+    genai.configure(api_key=os.getenv('GIMINI_API_KEY', None))
     response = model.generate_content(f"<請扮演一個有趣幽默不失智慧的人，像普通人一樣回覆訊息>{message}",safety_settings={'HARASSMENT':'block_none'})
     return response.text
 
