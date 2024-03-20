9859679529a95c92d97ee44b4f287878afde3728
powen <powen0966091716@gmail.com>
2024-03-20 19:38:31 +0800

    Update event_handler.py

--- handler/event_handler.py
+++ handler/event_handler.py
@@ -65,7 +65,7 @@ class EVENT_HANDLER:
             msg = msg.replace(pattern, '', 1)
             response = chat_ai.get_gpt_35_api_response(messages=[msg])
             cnt = chat_ai.read_open_api_usage_count()
-            response+=f"\n\n<< API_USAGE_COUNT >> :\n{cnt} times in this hour >>"
+            response+=f"\n\n<< API_USAGE_COUNT >>\n{cnt} times in this hour"
             return TextMessage(text=response)
 
     def handle(self):
