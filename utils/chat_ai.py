a4215549bfb5d9b3c5cdae40d033e2bb9e40c06e
powen <powen0966091716@gmail.com>
2024-03-21 10:11:03 +0800

    Update chat_ai.py

--- utils/chat_ai.py
+++ utils/chat_ai.py
@@ -24,7 +24,7 @@ safety_settings = [
 
 
 def get_ai_response(message: str) -> str:
-    model = genai.GenerativeModel('gemini-pro')
+    model = genai.GenerativeModel('gemini-1.0-pro-001')
     genai.configure(api_key=os.getenv('GIMINI_API_KEY', None))
     response = model.generate_content(
         f"<請扮演一個有趣幽默不失智慧的人，像普通人一樣回覆訊息，並快速給我簡短的回應>{message}", safety_settings=safety_settings)
